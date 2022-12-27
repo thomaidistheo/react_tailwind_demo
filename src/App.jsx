df47dca448575e1865c54cf9300db3a2356a730a
Thodoris Thomaidis <thodoris.thomaidis@gmail.com>
2022-12-27 20:06:39 +0200

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import styles from './style'
-import { Stats, Business, Billing, CardDeal, Testimonials, Clients, Cta, Footer } from './components'
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components'
 
 
 const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
 
       <div className={` bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
       <div className={` bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          Cta
-          Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer  />
         </div>
       </div>
     </div>
